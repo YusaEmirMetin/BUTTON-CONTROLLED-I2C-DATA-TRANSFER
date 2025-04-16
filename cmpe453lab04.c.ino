b55435f4a1295e1f3202a27449857cd29a5c69d5
YusaEmirMetin <162441414+YusaEmirMetin@users.noreply.github.com>
2025-04-16 15:02:45 +0300

    Add files via upload

---
+++ cmpe453lab04.c.ino
@@ -0,0 +1,54 @@
+#include <Wire.h>
+#include <rgb_lcd.h>
+
+/*
+Mert Koçoğlu
+Arda Ali Altıncı
+Mustafa Vanlıoğlu
+Yüşa Emir Metin
+Yaşar Düzgün
+*/
+
+
+rgb_lcd lcd;
+const int buttonPin = A3; 
+
+bool lastButtonState = LOW;
+
+void setup() {
+  
+  pinMode(buttonPin, INPUT);
+
+  // Initialize the LCD
+  lcd.begin(16, 2);
+
+  
+  lcd.setRGB(255, 0, 0);
+  lcd.print("CMPE453-2024F");
+  delay(3000);
+  lcd.clear();
+}
+
+void loop() {
+  
+  int buttonState = digitalRead(buttonPin);
+
+  if (buttonState == HIGH && lastButtonState == LOW) {
+    
+    lcd.clear();
+    lcd.print("Welcome to Class");
+    lcd.setCursor(0, 1);
+    lcd.print("Button is ON");
+  } else if (buttonState == LOW && lastButtonState == HIGH) {
+    // Button is released
+    lcd.clear();
+    lcd.print("GoodBye to Class");
+    lcd.setCursor(0, 1);
+    lcd.print("Button is OFF");
+  }
+
+  
+  lastButtonState = buttonState;
+
+  delay(100); 
+}
